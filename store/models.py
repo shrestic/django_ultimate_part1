9496ccd9612c64ebc775f0e5a0f88b8de8f917d5
shrestic <nhatphongcgp@gmail.com>
2025-02-27 18:08:55 +0700

    Creating Migrations

--- store/models.py
+++ store/models.py
@@ -15,8 +15,9 @@ class Collections(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now_add=True)
     collections = models.ForeignKey(Collections, on_delete=models.PROTECT)
