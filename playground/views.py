921028adcce241c8df10fa8c5a576e1f1f249a33
shrestic <nhatphongcgp@gmail.com>
2025-02-27 21:34:37 +0700

    Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from store.models import Product, Order
 from django.db.models import Q, F
 from django.shortcuts import render
 from store.models import Product
 
 
 def say_hello(request):
-    # Use only() to restrict the fields retrieved from the database
-    queryset = Product.objects.only("id", "title")
+    # queryset = Product.objects.select_related("collection__someOtherField").all()
+    # queryset = Product.objects.prefetch_related("promotions").select_related("collection").all()
+    queryset = (
+        Order.objects.prefetch_related("orderitem_set__product").select_related("customer").order_by("-placed_at")[:5]
+    )
 
-    # If 'description' is accessed later, ensure it was included in only() or use defer() carefully
-    # queryset = Product.objects.defer("description")  # This is redundant after only()
-
-    return render(request, "hello.html", {"name": "Mosh", "products": list(queryset)})
+    # return render(request, "hello.html", {"name": "Mosh", "products": list(queryset)})
+    return render(request, "hello.html", {"name": "Mosh", "orders": list(queryset)})
