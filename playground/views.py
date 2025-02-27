f993e904b33f92c5329f5698aa6e9f9c68228fb3
shrestic <nhatphongcgp@gmail.com>
2025-02-27 16:22:34 +0700

    debug django app in vscode

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def cal():
+    x = 1
+    y = 2
+    return x
+
+
 # Create your views here.
 def say_hello(request):
+    x = cal()
+    y = 2
     return render(request, "hello.html", {"name": "Phong"})
