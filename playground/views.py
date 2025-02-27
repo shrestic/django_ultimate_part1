140972afe0aae7d361dc954fca573552b0a4810d
shrestic <nhatphongcgp@gmail.com>
2025-02-27 20:48:10 +0700

    Complex Lookups Using Q Objects

--- playground/views.py
+++ playground/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
+from django.db.models import Q
 
 
 def say_hello(request):
-
-    queryset = Product.objects.filter(last_update__year=2021)
+    # Product: inventory < 10 and price < 20
+    # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    
+    # Product: inventory < 10 or price < 20
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    
 
     return render(request, "hello.html", {"name": "Mosh", "products": list(queryset)})
