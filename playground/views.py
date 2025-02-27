246c49b42eb343c9ceedcef18c401f727dcfe2d7
shrestic <nhatphongcgp@gmail.com>
2025-02-27 21:48:47 +0700

    Grouping Data

--- playground/views.py
+++ playground/views.py
@@ -9,17 +9,7 @@ from store.models import Product, Order, Customer
 
 
 def say_hello(request):
-    # queryset = Customer.objects.annotate(
-    #     #    full_name= F('first_name') + Value(' ') + F('last_name')
-    #     full_name=Func(
-    #         F("first_name"),
-    #         Value(" "),
-    #         F("last_name"),
-    #         function="CONCAT",
-    #     )
-    # )
-    queryset = Customer.objects.annotate(
-        full_name=Concat("first_name", Value(" "), "last_name")
-    )
+    # order_set not working
+    queryset = Customer.objects.annotate(orders_count=Count("order"))
 
     return render(request, "hello.html", {"name": "Mosh", "result": queryset})
