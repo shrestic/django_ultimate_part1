e74f8220ad2798cb3ef2ae237c1fccadcc66cb31
shrestic <nhatphongcgp@gmail.com>
2025-02-28 00:56:10 +0700

    Querying Generic Relationships

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F, Value, Func, ExpressionWrapper,DecimalField
+from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
 from django.db.models.aggregates import Count, Sum, Avg, Max, Min
 from django.db.models.functions import Concat
 from store.models import Product, Order, Customer
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    # Cannot infer type of '*' expression involving these types: DecimalField, FloatField. You must set output_field.
-    # queryset = Product.objects.annotate(discounted_price=F("unit_price") * 0.8)
-    discounted_price = ExpressionWrapper(F("unit_price") * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(discounted_price=discounted_price)
+    content_type = ContentType.objects.get_for_model(Product)
+    queryset = TaggedItem.objects.select_related("tag").filter(
+        content_type=content_type,
+        object_id=1,
+    )
 
-    return render(request, "hello.html", {"name": "Mosh", "result": queryset})
+    return render(request, "hello.html", {"name": "Mosh", "result": list(queryset)})
