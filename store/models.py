6a036c166529e00efb4ab20117cc54c3c0df81bd
shrestic <nhatphongcgp@gmail.com>
2025-02-27 17:28:26 +0700

    Defining a One-to-many Relationship

--- store/models.py
+++ store/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
-
 # Create your models here.
+
+
+class Collections(models.Model):
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now_add=True)
+    collections = models.ForeignKey(Collections, on_delete=models.PROTECT)
 
 
 class Customer(models.Model):
@@ -39,9 +45,31 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    """
+    The cart field is a ForeignKey linking CartItem to Cart, meaning each CartItem belongs to one Cart, but a Cart can have multiple CartItems.
+    on_delete=models.CASCADE: If a Cart is deleted, all related CartItem instances will also be deleted. This makes sense because when a shopping cart is removed, its items should be removed too.
+    """
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    """
+    The product field is a ForeignKey linking CartItem to Product, meaning each CartItem is associated with one Product, but a Product can be in multiple CartItems (i.e., in different carts).
+    on_delete=models.PROTECT: This prevents a Product from being deleted if it is referenced in any CartItem. This makes sense because you don’t want to remove a product that customers have in their carts.
+    """
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
