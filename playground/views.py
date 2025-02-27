5757a6d9f9d25ea7a863d1ebdcb81b4689e13109
shrestic <nhatphongcgp@gmail.com>
2025-02-27 21:45:52 +0700

    Calling Database Functions

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F, Value
+from django.db.models import Q, F, Value, Func
 from django.shortcuts import render
 from django.db.models.aggregates import Count, Sum, Avg, Max, Min
+from django.db.models.functions import Concat
 from store.models import Product, Order, Customer
 
 
 def say_hello(request):
-    # queryset = Customer.objects.annotate(is_new=Value(True))
-    queryset = Customer.objects.annotate(is_new=F("id"))
+    # queryset = Customer.objects.annotate(
+    #     #    full_name= F('first_name') + Value(' ') + F('last_name')
+    #     full_name=Func(
+    #         F("first_name"),
+    #         Value(" "),
+    #         F("last_name"),
+    #         function="CONCAT",
+    #     )
+    # )
+    queryset = Customer.objects.annotate(
+        full_name=Concat("first_name", Value(" "), "last_name")
+    )
 
     return render(request, "hello.html", {"name": "Mosh", "result": queryset})
