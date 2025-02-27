6ee65019d88c32a176ce17fd5126ccd44a35bb73
shrestic <nhatphongcgp@gmail.com>
2025-02-28 01:07:06 +0700

    Understanding QuerySet Cache

--- playground/views.py
+++ playground/views.py
@@ -11,10 +11,8 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    # content_type = ContentType.objects.get_for_model(Product)
-    # queryset = TaggedItem.objects.select_related("tag").filter(
-    #     content_type=content_type,
-    #     object_id=1,
-    # )
-    queryset = TaggedItem.objects.get_tags_for(Product, 1)
-    return render(request, "hello.html", {"name": "Mosh", "result": list(queryset)})
+    queryset = Product.objects.all()
+    queryset[0]
+    list(queryset)
+    # End up two separated queries
+    return render(request, "hello.html", {"name": "Mosh"})
