1515d489d3b9293bf788243c0a90a9a85e181b8a
shrestic <nhatphongcgp@gmail.com>
2025-02-28 16:16:02 +0700

    Adding Filtering to the List Page

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,40 @@
 from . import models
-from django.db.models import Count
+from django.db.models import Count, QuerySet
 from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
 
-
 # Register your models here.
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    """
+    Custom filter for Django admin to filter products based on inventory levels.
+
+    Attributes:
+        title (str): The title of the filter displayed in the admin interface.
+        parameter_name (str): The parameter name used in the URL query.
+
+    Methods:
+        lookups(request, model_admin):
+            Returns a list of tuples containing the filter options and their display names.
+
+        queryset(request, queryset: QuerySet):
+            Filters the queryset based on the selected filter option.
+            If the selected option is "<10", it filters the queryset to include only items with inventory less than 10.
+    """
+
+    title = "inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [("<10", "Low")]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ["title", "products_count"]
@@ -31,9 +60,10 @@ class ProductAdmin(admin.ModelAdmin):
     # collection__title not work
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
-    list_per_page = 10
     # prevent extra queries (from 15 queries down to 5 queries)
     list_select_related = ["collection"]
+    list_filter = ["collection", "last_update", InventoryFilter]
+    list_per_page = 10
 
     def collection_title(self, product: models.Product):
         return product.collection.title
@@ -52,7 +82,6 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ["first_name__istartswith", "last_name__istartswith"]
     ordering = ["first_name", "last_name"]
-    
 
 
 @admin.register(models.Order)
